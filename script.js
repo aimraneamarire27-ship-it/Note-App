ee819334c96b2ac1262e59b64cd02497a71e6b22
aimraneamarire27-ship-it <aimraneamarire27@gmail.com>
2025-09-07 16:00:04 +0100

    Version 1

---
+++ script.js
@@ -0,0 +1,67 @@
+const color = document.getElementById('color');
+const addNoteBtn = document.querySelector('.add-note');
+const listEl = document.querySelector('.list');
+
+
+document.addEventListener('click',(e)=>{
+  if(e.target.classList.contains('close')){
+    e.target.parentNode.remove()
+  }
+})
+
+addNoteBtn.addEventListener('click',()=>{
+ let targetColor = color.value;
+ let noteHTML = `
+  <div class="note">
+    <textarea style='  border-top:30px solid ${targetColor} ;' placeholder="Write Content ..." name="note" id="note-text"></textarea>
+    <span class="material-symbols-outlined close">close</span>
+  </div>
+  `
+  listEl.innerHTML += noteHTML;
+})
+
+let cursor = {
+  x:null,
+  y:null
+}
+
+let elementData = {
+  dom:null,
+  x:null,
+  y:null
+}
+let lastZIndex = 1;
+document.addEventListener('mousedown',(e)=>{
+  if(e.target.parentNode.classList.contains('note')){
+    elementData = {
+      dom : e.target.parentNode,
+      x : e.target.getBoundingClientRect().left,
+      y : e.target.getBoundingClientRect().top
+    }
+    cursor = {
+      x : e.clientX,
+      y : e.clientY
+    }
+    elementData.dom.style.zIndex = `${lastZIndex}`;
+    lastZIndex++
+ }
+})
+document.addEventListener('mousemove',(e)=>{
+  if(elementData.dom == null) return;
+  let newCursor = {
+    x : e.clientX,
+    y : e.clientY 
+  }
+  let distance = {
+    x : newCursor.x - cursor.x,
+    y : newCursor.y - cursor.y
+  }
+  
+  elementData.dom.style.top = (distance.y + elementData.y) + 'px';
+  elementData.dom.style.left = (distance.x + elementData.x) + 'px';
+
+});
+
+document.addEventListener('mouseup' , ()=>{
+  elementData.dom = null
+})
